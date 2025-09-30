bf7cc456fbcf4419cc8683a13d910785ecc7be0c
itsidrokin28 <itsidorkin28@gmail.com>
2025-09-30 20:25:41 +0300

    Добавлен параметр disabled в компонент StarsCount для управления состоянием кнопки, изменена структура компонента на кнопку с улучшением стилей и адаптивности. Исправлен атрибут clip-path на clipPath в StarIcon для соответствия стандартам JSX.

--- src/components/StarsCount.tsx
+++ src/components/StarsCount.tsx
@@ -2,15 +2,20 @@ import { StarIcon } from './icons';
 
 type StarsCountProps = {
   number: number;
+  disabled?: boolean;
 };
 
-const StarsCount = ({ number }: StarsCountProps) => (
-  <div className="flex flex-row gap-0 items-center justify-center text-white border-white border-[1px] rounded-[12px] px-2 min-h-[45px]">
-    <div>
+const StarsCount = ({ number, disabled }: StarsCountProps) => (
+  <button
+    type="button"
+    disabled={disabled}
+    className="relative overflow-hidden flex flex-row gap-0 items-center justify-center text-white border-white border-[1px] rounded-[12px] min-h-[45px] w-[131px]"
+  >
+    <div className="border-r-[1px] border-t-[1px] border-b-[1px] border-white flex items-center justify-center rounded-[12px] absolute left-0 min-h-[45px] w-[56px]">
       <StarIcon />
     </div>
-    <span className="text-2">{number}</span>
-  </div>
+    <span className="text-2 absolute right-[18px]">{number}</span>
+  </button>
 );
 
 export default StarsCount;
